86e6fe6de4be2b00ec875ecf56cc83654c00062b
Jordan Oos <imortalstrike15@gmail.com>
2024-09-11 12:02:37 +0200

    Fixed C++ File Error

---
+++ GameDevFPSGame/Source/GameDevFPSGame/Class2.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Class2.h"
+
+Class2::Class2()
+{
+}
+
+Class2::~Class2()
+{
+}
---
+++ GameDevFPSGame/Source/GameDevFPSGame/Class2.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GAMEDEVFPSGAME_API Class2
+{
+public:
+	Class2();
+	~Class2();
+};
