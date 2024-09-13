cc5da3cec02658242681fd19873d5ea1eef2d403
Jordan Oos <imortalstrike15@gmail.com>
2024-09-13 18:38:05 +0200

    Movement Code Implemented

---
+++ GameDevFPSGame/Source/GameDevFPSGame/PlayerControllerClass.cpp
@@ -0,0 +1,106 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerControllerClass.h"
+
+#include "K2Node_GetSubsystem.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+
+void APlayerControllerClass::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	PlayerCharacter = Cast<AMyCharacter>(InPawn);
+	checkf(PlayerCharacter, TEXT("APlayerControllerB derived Class should only posses AMyCharacter derived Pawns"))
+
+	EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	checkf(EnhancedInputComponent, TEXT("Uable to find a refernece to the EnchancecLocalPlayerSubsystem"))
+
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	checkf(InputSubsystem, TEXT("Unable to get a reference to the EnhancedPlayerSystem"))
+
+
+	checkf(InputMappingContext, TEXT("Input Mapping was not Specified"))
+
+	InputSubsystem ->ClearAllMappings();
+	InputSubsystem->AddMappingContext(InputMappingContext, 0);
+
+
+	if(ActionMovement)
+	{
+		EnhancedInputComponent->BindAction(ActionMovement, ETriggerEvent::Triggered, this, &APlayerControllerClass::HandleMove);
+	}
+
+	if(ActionLookAround)
+	{
+		EnhancedInputComponent->BindAction(ActionLookAround, ETriggerEvent::Triggered, this, &APlayerControllerClass::HandleLookAround);
+	}
+
+	if(ActionJump)
+	{
+		EnhancedInputComponent->BindAction(ActionJump, ETriggerEvent::Triggered, this, &APlayerControllerClass::HandleJump);
+	}
+
+	if(ActionCrouch)
+	{
+		EnhancedInputComponent->BindAction(ActionCrouch, ETriggerEvent::Triggered, this, &APlayerControllerClass::HandleCrouch);
+	}
+
+	
+	
+}
+
+void APlayerControllerClass::OnUnPossess()
+{
+	EnhancedInputComponent->ClearActionBindings();
+
+	Super::OnUnPossess();
+}
+
+void APlayerControllerClass::HandleCrouch()
+{
+	if(PlayerCharacter->GetMovementBase())
+	{
+
+		
+     PlayerCharacter->GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
+		
+		
+	}
+
+	PlayerCharacter->GetMovementComponent()->IsCrouching() ? PlayerCharacter->UnCrouch(true) : PlayerCharacter->Crouch(false);
+		
+		
+	
+}
+
+
+
+void APlayerControllerClass::HandleLookAround(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2d>();
+
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
+
+void APlayerControllerClass::HandleMove(const FInputActionValue& InputActionValue)
+{
+	const FVector2D MovementVector = InputActionValue.Get<FVector2d>();
+
+	if(PlayerCharacter)
+	{
+		PlayerCharacter->AddMovementInput(PlayerCharacter->GetActorForwardVector(), MovementVector.Y);
+		PlayerCharacter->AddMovementInput(PlayerCharacter->GetActorRightVector(), MovementVector.X);
+	}
+}
+
+void APlayerControllerClass::HandleJump()
+{
+	if(PlayerCharacter)
+	{
+		PlayerCharacter->Jump();
+		PlayerCharacter->UnCrouch();
+	}
+}
---
+++ GameDevFPSGame/Source/GameDevFPSGame/PlayerControllerClass.h
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputSubsystems.h"
+//#include "Engine/Source/Runtime/EnhancedInput/Public/EnhancedInputComponent.h"
+#include "MyCharacter.h"
+#include "GameFramework/PlayerController.h"
+#include "EnhancedInputDeveloperSettings.h"
+#include "EnhancedInputSubsystems.h"
+#include  "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+#include "InputAction.h"
+#include "PlayerControllerClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class GAMEDEVFPSGAME_API APlayerControllerClass : public APlayerController
+{
+public: 
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input | Character Movement")
+	UInputAction* ActionMovement = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input | Character Movement")
+	UInputAction* ActionLookAround = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input | Character Movement")
+	UInputAction* ActionJump = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input | Character Movement")
+	UInputAction* ActionCrouch = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input | Character Movement")
+	TObjectPtr<UInputMappingContext> InputMappingContext = nullptr;
+
+
+protected:
+
+	virtual  void OnPossess(APawn* InPawn) override;
+
+	virtual void OnUnPossess() override;
+
+	void HandleLookAround(const FInputActionValue& InputActionValue);
+
+	void HandleMove(const FInputActionValue& InputActionValue);
+
+	void HandleJump();
+
+	void HandleCrouch();
+	
+	
+private:
+
+	UPROPERTY()
+	UEnhancedInputComponent* EnhancedInputComponent = nullptr;
+
+	UPROPERTY()
+	AMyCharacter* PlayerCharacter = nullptr;
+
+	
+	GENERATED_BODY()
+     
+};
