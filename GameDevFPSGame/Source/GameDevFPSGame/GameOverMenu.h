a45d7db7697adb890617843643504fb107298f82
Jordan Oos <imortalstrike15@gmail.com>
2024-11-23 15:07:47 +0200

    Pause Menu and Main Menu Made

---
+++ GameDevFPSGame/Source/GameDevFPSGame/GameOverMenu.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GameOverMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEDEVFPSGAME_API UGameOverMenu : public UUserWidget
+{
+	GENERATED_BODY()
+};
---
+++ GameDevFPSGame/Source/GameDevFPSGame/PauseMenu.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PauseMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEDEVFPSGAME_API UPauseMenu : public UUserWidget
+{
+	GENERATED_BODY()
+};
