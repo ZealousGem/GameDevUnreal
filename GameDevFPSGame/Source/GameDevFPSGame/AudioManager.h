42f814335dab4a8a31d91d9ede90f64f9e13c439
Jordan Oos <imortalstrike15@gmail.com>
2024-11-25 18:10:15 +0200

    Made AudioManager

---
+++ GameDevFPSGame/Source/GameDevFPSGame/AudioManager.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AudioManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEDEVFPSGAME_API UAudioManager : public UObject
+{
+	GENERATED_BODY()
+};
